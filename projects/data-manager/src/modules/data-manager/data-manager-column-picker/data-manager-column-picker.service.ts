b0334dfaf78e93bcc47d87d054c0d752630d4f71
Paul Crowder <paul.crowder@blackbaud.com>
2021-09-01 16:41:50 -0400

    Update data-manager-column-picker.service.ts

--- projects/data-manager/src/modules/data-manager/data-manager-column-picker/data-manager-column-picker.service.ts
+++ projects/data-manager/src/modules/data-manager/data-manager-column-picker/data-manager-column-picker.service.ts
@@ -6,6 +6,12 @@ import type {
   SkyDataManagerColumnPickerComponent
 } from './data-manager-column-picker.component';
 
+/**
+ * Service that provides the column picker component type so it can be dynamically
+ * created in the toolbar component. This avoids a circular reference between column
+ * picker and toolbar.
+ * https://angular.io/errors/NG3003
+ */
 @Injectable()
 export abstract class SkyDataManagerColumnPickerService {
   public abstract getComponentType(): typeof SkyDataManagerColumnPickerComponent;
