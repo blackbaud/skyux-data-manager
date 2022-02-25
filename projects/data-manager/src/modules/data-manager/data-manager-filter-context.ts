bed63954104296b0f29ed6906b793f1cd8542ee0
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2022-02-25 13:37:48 -0500

    Updated README.md to point contributors to the monorepo [ci skip]

--- projects/data-manager/src/modules/data-manager/data-manager-filter-context.ts
+++
@@ -1,5 +0,0 @@
-import { SkyDataManagerFilterData } from './models/data-manager-filter-data';
-
-export class SkyDataManagerFilterModalContext {
-  public filterData: SkyDataManagerFilterData;
-}
--- projects/data-manager/src/modules/data-manager/models/data-manager-column-picker-sort-strategy.ts
+++
@@ -1,4 +0,0 @@
-export enum SkyDataManagerColumnPickerSortStrategy {
-  None = 'none',
-  SelectedThenAlphabetical = 'selectedThenAlphabetical',
-}
